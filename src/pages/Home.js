ed04a8ded83099519297332d6d9b06a34734d9a7
Yaakov <dbrionesmtz@yahoo.com>
2021-11-01 17:53:43 -0600

    add target blank

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,7 +18,7 @@ const Home = () => {
                 </div>
                 <div class="curly-brace-content-row">
                     <span class="key-blue">&#34;contacto:&#34;</span>
-                    <span class="value-white">&#91;&#34;<a href="https://github.com/yaakov04" target="blank">Github</a>&#34;, &#34;<a href="https://www.linkedin.com/in/diego-b-7a6121206/" target="blank">Linkedin</a>&#34;, &#34;<a href="mailto:dbrionesmtz@yahoo.com" target="blank">Email</a>&#34;&#93;</span>
+                    <span class="value-white">&#91;&#34;<a href="https://github.com/yaakov04" target="_blank" rel="noopener noreferrer">Github</a>&#34;, &#34;<a href="https://www.linkedin.com/in/diego-b-7a6121206/" target="_blank" rel="noopener noreferrer">Linkedin</a>&#34;, &#34;<a href="mailto:dbrionesmtz@yahoo.com" target="_blank" rel="noopener noreferrer">Email</a>&#34;&#93;</span>
                 </div>
             </div>
             <span class="curly-brace">&#125;</span>
