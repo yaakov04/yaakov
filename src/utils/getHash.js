f715f24bb17929e1307d43866df30ee962a7fbb5
Yaakov <dbrionesmtz@yahoo.com>
2021-11-01 13:13:51 -0600

    modified getHash function to get other parameters

--- src/utils/getHash.js
+++ src/utils/getHash.js
@@ -1,3 +1,3 @@
-const getHash = () => location.hash.slice(1).toLocaleLowerCase().split('/')[1] || '/';
+const getHash = (index = 1) => location.hash.slice(1).toLocaleLowerCase().split('/')[index] || '/';
 
 export default getHash;
