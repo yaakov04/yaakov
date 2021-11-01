1546d7fad1a6c3f4d3f0fd20cfe99ea97942c700
Yaakov <dbrionesmtz@yahoo.com>
2021-11-01 16:41:33 -0600

    import json

--- src/pages/Portafolio.js
+++ src/pages/Portafolio.js
@@ -1,44 +1,9 @@
 import projectsPerPage from "../utils/projectsPerPage";
 import getHash from "../utils/getHash";
+import allProjects from "../json/projects.json";
 
 const Portafolio = () => {
-        const allProjects = [{
-            'id': 1,
-            'nombre': 'Proyecto 1'
-        }, {
-            'id': 2,
-            'nombre': 'Proyecto 2'
-        }, {
-            'id': 3,
-            'nombre': 'Proyecto 3'
-        }, {
-            'id': 4,
-            'nombre': 'Proyecto 4'
-        }, {
-            'id': 5,
-            'nombre': 'Proyecto 5'
-        }, {
-            'id': 6,
-            'nombre': 'Proyecto 6'
-        }, {
-            'id': 7,
-            'nombre': 'Proyecto 7'
-        }, {
-            'id': 8,
-            'nombre': 'Proyecto 8'
-        }, {
-            'id': 9,
-            'nombre': 'Proyecto 9'
-        }, {
-            'id': 10,
-            'nombre': 'Proyecto 10'
-        }, {
-            'id': 11,
-            'nombre': 'Proyecto 11'
-        }, {
-            'id': 12,
-            'nombre': 'Proyecto 12'
-        }, ];
+        //const allProjects = null;
         const resultsPerPage = 6;
         const currentPage = parseInt(getHash(2));
         const allPage = allProjects.length / resultsPerPage;
