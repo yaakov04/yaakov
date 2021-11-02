b2c74156e7f14d9243dbbcd9b615d32a7abf7eaf
Yaakov <dbrionesmtz@yahoo.com>
2021-11-02 13:13:41 -0600

    add validation

--- src/utils/projectsPerPage.js
+++ src/utils/projectsPerPage.js
@@ -1,7 +1,7 @@
 const projectsPerPage = (projects, resultsPerPage, currentPage) => {
     const projectsPerPage = [];
     for (let i = (currentPage - 1) * resultsPerPage; i < resultsPerPage * currentPage; i++) {
-        projectsPerPage.push(projects[i]);
+        if (projects[i]) { projectsPerPage.push(projects[i]); }
     }
     return projectsPerPage;
 }
