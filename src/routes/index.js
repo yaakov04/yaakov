f053784214bed32853352debfce659fcc52ae73a
Yaakov <dbrionesmtz@yahoo.com>
2021-11-03 12:18:41 -0600

    add mobile menu

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import Header from '../template/Header';
+import MobileMenu from '../template/MobileMenu';
 import Error404 from '../pages/Error404';
 import Home from "../pages/Home";
 import Portafolio from '../pages/Portafolio';
@@ -15,8 +16,10 @@ const routes = {
 const router = async() => {
     const header = null || document.querySelector('#header');
     const content = null || document.querySelector('#content');
+    const mobileMenu = null || document.querySelector('.Mobile-menu');
 
     header.innerHTML = await Header();
+    mobileMenu.innerHTML = await MobileMenu();
     let hash = getHash();
     let route = await resolveRoutes(hash);
     let render = routes[route] ? routes[route] : Error404;
---
+++ src/template/MobileMenu.js
@@ -0,0 +1,11 @@
+const MobileMenu = () => {
+    const view = `
+        <nav>
+            <a href="#"><i class="fas fa-home"></i></a>
+            <a href="#/portafolio/1"><i class="fas fa-th-large"></i></a>
+        </nav>
+    `;
+    return view;
+};
+
+export default MobileMenu;
