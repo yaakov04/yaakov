17c12111019e40f18ec56567419a06524abebd35
Yaakov <dbrionesmtz@yahoo.com>
2021-11-03 12:36:06 -0600

    update gh-pages

--- src/utils/projectsPerPage.js
+++
@@ -1,9 +0,0 @@
-const projectsPerPage = (projects, resultsPerPage, currentPage) => {
-    const projectsPerPage = [];
-    for (let i = (currentPage - 1) * resultsPerPage; i < resultsPerPage * currentPage; i++) {
-        if (projects[i]) { projectsPerPage.push(projects[i]); }
-    }
-    return projectsPerPage;
-}
-
-export default projectsPerPage;
