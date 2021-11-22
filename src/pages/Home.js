4e33fd5151d3a83914753529312a5cde76947bea
Yaakov <dbrionesmtz@yahoo.com>
2021-11-21 20:35:42 -0600

    add projects and add styles to desktop view

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ const Home = () => {
     const view = `
         <section class="section-inicio entry">
             <h2>Diego <span class="key-blue">Briones</span></h2>
-            <h3>Backend Developer</h3>
+            <h3>Web Developer</h3>
             <nav>
                 <a href="https://github.com/yaakov04" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
                 <a href="https://www.linkedin.com/in/diego-b-7a6121206/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i></a>
